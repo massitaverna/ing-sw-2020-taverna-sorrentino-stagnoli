aa0732ce888197f6689e5ed5b6a85335c3194340
Nicolò Stagnoli <10578583@polimi.it>
2020-03-17 17:37:55 +0100

    Aggiunto Model.java

--- src/main/java/it/polimi/ingsw/model/Model.java
+++ src/main/java/it/polimi/ingsw/model/Model.java
@@ -1,4 +1,25 @@
 package it.polimi.ingsw.model;
 
 public class Model {
+
+    public void SetChoosenGods(God[] list){
+
+    }
+
+    public void SetStartingPlayerChoose(Player p){
+
+    }
+
+    public void SetPlayerWorkerChose(Player p, Worker w){
+
+    }
+
+    public void SetPlayerMoveChose(Player p, Move m){
+
+    }
+
+    public void SetPlayerBuildChose(Player p, Move b){
+
+    }
+
 }
