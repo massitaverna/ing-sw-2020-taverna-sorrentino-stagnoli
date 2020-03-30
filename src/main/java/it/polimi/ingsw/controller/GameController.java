6f52f1821e438bc945fe667c415fc5e842d5dec1
Nicolò Stagnoli <10578583@polimi.it>
2020-03-30 15:41:10 +0200

    Aggiunta interfaccia ChallengerViewEventListener;

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -5,13 +5,4 @@ import it.polimi.ingsw.model.GameModel;
 
 public class GameController {
 
-    private PlayerChoseBuildListener PCBL;
-    private PlayerChoseColorListener PCCL;
-    private PlayerChoseMoveListener PCML;
-    private PlayerChoseWorkerListener PCWL;
-
-    private GameModel model;
-
-    public GameController(){
-        this.model = new GameModel();    }
 }
