32d6d9272b963a517019d50301e457db7f82f640
Nicolò Stagnoli <10578583@polimi.it>
2020-03-31 13:43:03 +0200

    Aggiornato UML package model; Aggiunto Sequence Diagram - Turno di Gioco.drawio;

--- src/main/java/it/polimi/ingsw/listeners/Model/ModelEventListener.java
+++ src/main/java/it/polimi/ingsw/listeners/Model/ModelEventListener.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.listeners.Model;
 public interface ModelEventListener {
     public void onAllPlayersArrived();
     public void onBoardChanged();
-    // public void onColorChosen();  I don't think this method is useful
+    public void onColorChosen();  //serve per far scegliere i colori secondo l'ordine di gioco in modo da evitare confilitti
     public void onGameReadyListener();
     public void onGodsChosenListener();
     public void onPlayerAddedListener(String nickname);
